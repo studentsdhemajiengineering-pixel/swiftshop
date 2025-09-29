4b7d442a327855145bea4988bad7283a8121b3d8
Students DhemajiEngineering <studentsdhemajiengineering@gmail.com>
2025-09-29 11:25:36 +0000

    try show 3 products like this but dont copy stye from this image i just

--- src/components/products/product-carousel.tsx
+++ src/components/products/product-carousel.tsx
@@ -28,7 +28,7 @@ export function ProductCarousel({ products }: ProductCarouselProps) {
     >
       <CarouselContent>
         {products.map((product) => (
-          <CarouselItem key={product.id} className="basis-[48%] sm:basis-1/3 md:basis-1/4 lg:basis-1/5">
+          <CarouselItem key={product.id} className="basis-1/3 sm:basis-1/3 md:basis-1/4 lg:basis-1/5">
             <ProductCard product={product} />
           </CarouselItem>
         ))}
